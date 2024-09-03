16a963115c9e9141ae973d76c5ed805e4fda7f37
Tois286 <itoto1937@gmail.com>
2024-09-04 03:47:01 +0700

    push 2

---
+++ module/content.php
@@ -0,0 +1,141 @@
+<style>
+    .card {
+        box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.2);
+        display: flex;
+        flex-direction: column;
+        height: 97%;
+    }
+
+    .card-body {
+        flex-grow: 1;
+    }
+
+    .card-body ul {
+        padding-left: 20px;
+    }
+
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .col-md-4 {
+        display: flex;
+    }
+</style>
+
+<body>
+
+    <div class="container mt-5">
+        <div class="jumbotron text-center">
+            <h1 class="display-4">Welcome to Our Binus Order!</h1>
+            <p class="lead">Discover the best products at unbeatable prices.</p>
+            <a class="btn btn-primary btn-lg" href="view/shop.php" role="button">Shop Now</a>
+        </div>
+
+        <div class="row">
+            <!-- Product Card 1 -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="assets/binus/laptop.jpg" class="card-img-top" alt="Product 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Binus (Bisnis Usaha Bagian A)</h5>
+                        <p>Halo sobat danus☺👋🏻 Laptop kalian mengalami trouble? Atau hp kalian juga mengalami trouble? Langsung saja hubungi kami. Kami siap melayani:</p>
+                        <ul>
+                            <li>Install/repair OS</li>
+                            <li>Troubleshooting</li>
+                            <li>Backup data HDD/SSD</li>
+                            <li>Maintenance</li>
+                            <li>Network settings</li>
+                            <li>Replace software and hardware</li>
+                            <li>Dll</li>
+                        </ul>
+                    </div>
+                    <div class="card-footer text-center">
+                        <?php include 'module/sosmed.php' ?>
+                        <h6>Kujungi Saluran</h6>
+                        <button type="button" class="btn btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"></path>
+                            </svg>
+                            Kunjugi
+                        </button> <a href="view/repair.php" class="btn btn-primary">Repair Now</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Product Card 2 -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="assets/binus/website.jpg" class="card-img-top" alt="Product 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Binus (Bisnis Usaha Bagian B)</h5>
+                        <p>Halo sobat danus😊👋🏻 Kalian bingung ingin membuat website portofolio? Kami melayani segala macam pembuatan website:</p>
+                        <ul>
+                            <li>Website sekolah</li>
+                            <li>Website perusahaan</li>
+                            <li>Website toko online</li>
+                            <li>Website company profile</li>
+                            <li>Website individu</li>
+                            <li>Website portofolio</li>
+                            <li>Dll</li>
+                        </ul>
+                        <p>Tunggu apalagi? Yuk pesan sekarang🤩</p>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <?php include 'module/sosmed.php' ?>
+                        <h6>Kujungi Saluran</h6>
+
+                        <button type="button" class="btn btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"></path>
+                            </svg>
+                            Kunjugi
+                        </button> <a href="#" class="btn btn-primary">Web Service Now</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Product Card 3 -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="assets/binus/design.jpg" class="card-img-top" alt="Product 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Binus (Bisnis Usaha Bagian C)</h5>
+                        <p>Halo sobat danus☺👋🏻 Kalian bingung desain feed IG menarik? Atau ingin membuat logo bisnis? Kami membuka jasa desain:</p>
+                        <ul>
+                            <li>Desain spanduk</li>
+                            <li>Desain logo</li>
+                            <li>Desain kaos</li>
+                            <li>Desain feed IG</li>
+                            <li>Desain brosur</li>
+                            <li>Desain kartu nama</li>
+                            <li>Desain undangan</li>
+                            <li>Dll</li>
+                        </ul>
+                        <p>Harga mulai dari Rp10.000 aja lohh🤩</p>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <?php include 'module/sosmed.php' ?>
+                        <h6>Kujungi Saluran</h6>
+
+                        <button type="button" class="btn btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"></path>
+                            </svg>
+                            Kunjugi
+                        </button> <a href="#" class="btn btn-primary">Design Service Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ module/footer.php
@@ -0,0 +1,7 @@
+ <footer class="bg-light text-center text-lg-start">
+     <div class="container p-4">
+         <div class="text-center p-3">
+             Binus© 2024 Servis Order.
+         </div>
+     </div>
+ </footer>
