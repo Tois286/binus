206409b65989e51a68398dcba6077c98856c4941
XinZhie151 <newstar1928@gmail.com>
2024-09-07 02:42:24 +0700

    Penambahan Logo Brand dan Editing dalam shop.php
    logo brand untuk menkategorikan lebih lanjut barang atau merk brand yang diinginkan oleh client

---
+++ view/shop.php
@@ -0,0 +1,93 @@
+<head>
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body style="background-color: #FFFFFF;">
+
+    <!-- Header -->
+        <?php include '../module/header.php' ?>
+
+    <!-- Content -->
+     <!-- Banner Atas -->
+    <div class="container mt-5">
+        <div class="jumbotron" style="background-image: url('../assets/binus/bg2.png'); background-size: cover; background-position: center;">
+            <div class="text-left" style="color:white;">
+                <h2 class="display-4">Welcome to Our Binus Shop!</h2>
+                <p class="lead">Find a laptop within your minimum budget and consult with experts here</p>
+                <a class="btn btn-primary btn-lg" href="https://wa.me/8568428849" role="button">Consult Now</a>
+            </div>
+        </div>
+
+     <!-- Category -->   
+        <div class="container p-4">
+            <div class="text-center p-3">
+                <h1>Category</h1>
+            </div>
+        </div>
+     
+    <div class="row">    
+        <!-- Product Card 1 -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/thinkpad.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+
+        <!-- Product Card 2 -->
+        <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/dell.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+        
+        <!-- Product Card 3 -->
+        <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/hp.jpg" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+        
+         <!-- Product Card 4 -->
+         <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/acer.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+
+        <!-- Product Card 5 -->
+        <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/lenovo.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+
+        <!-- Product Card 6 -->
+        <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/asus.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+
+        <!-- Product Card 7 -->
+        <div class="col-md-4">
+                <div class="card mb-4">
+                    <a href="https://www.google.com">
+                        <img src="../assets/binus/infinix.png" class="card-img-top" alt="Product 1">
+                    </a>
+                </div>
+            </div>
+    </div>
+    <!-- Footer -->
+        <?php include '../module/footer.php' ?>
+</body>
