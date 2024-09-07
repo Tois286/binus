a5d4b04e7d972c7e433139d8763f32823893f44f
Tois286 <itoto1937@gmail.com>
2024-09-07 13:06:34 +0700

    perbaikan job

--- module/footer.php
+++ module/footer.php
@@ -1,6 +1,7 @@
  <footer class="bg-light text-center text-lg-start">
      <div class="container p-4">
          <div class="text-center p-3">
+             Support By <a href="https://itcybercommunity.tois.my.id/">itcybercommunity</a><br>
              Binus© 2024 Servis Order.
          </div>
      </div>
