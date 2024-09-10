17b864ca6ec406a30396e8eb206ace2a23bfa099
Tois286 <itoto1937@gmail.com>
2024-09-10 13:21:45 +0700

    konflik hadi

--- view/shop.php
+++ view/shop.php
@@ -1,46 +1,154 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
+<<<<<<< HEAD
+    <!-- <head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    </head> -->
 
-<body style="background-color: #FFFFFF;">
+    <body style="background-color: #FFFFFF;">
+        =======
 
-    <!-- Header -->
-    <?php include '../module/header.php' ?>
-    <br>
-    <!-- Content -->
-    <!-- Banner Atas -->
-    <div class="slideshow">
-        <img class="slides" src="../assets/binus/thinkpad.png">
-        <img class="slides" src="../assets/binus/hp.jpg">
-        <img class="slides" src="../assets/binus/lenovo.png">
-        <img class="slides" src="../assets/binus/acer.png">
-        <img class="slides" src="../assets/binus/asus.png">
-        <img class="slides" src="../assets/binus/infinix.png">
-        <img class="slides" src="../assets/binus/dell.png">
-    </div>
-    <script src="../assets/js/script.js"></script>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="../assets/css/style.css">
+        </head>
 
-    <div class="form-container">
-        <h1 class="form-title">Contact Us</h1>
-        <form id="contact-form" class="contact-form">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" id="name" name="name" class="form-input" required>
+        <body style="background-color: #FFFFFF; background-attachment: fixed; background-size: cover; background-position: center;" background="../assets/binus/shopbg.jpg">
+            >>>>>>> 3941e5cf80c2bf52496ad2c08c6406365931b9c4
 
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" id="email" name="email" class="form-input" required>
+            <!-- Header -->
+            <?php include '../module/header.php' ?>
+            <br>
+            <!-- Content -->
+            <<<<<<< HEAD
+                <!-- Banner Atas -->
+                <div class="container mt-5">
+                    <div class="jumbotron" style="background-image: url('../assets/binus/bg2.png'); background-size: cover; background-position: center;">
+                        <div class="text-left" style="color:white;">
+                            <h2 class="display-4">Welcome to Our Binus Shop!</h2>
+                            <p class="lead">Find a laptop within your minimum budget and consult with experts here</p>
+                            <a class="btn btn-primary btn-lg" href="https://wa.me/8568428849" role="button">Consult Now</a>
+                        </div>
+                    </div>
 
-            <label for="message" class="form-label">Message:</label>
-            <textarea id="message" name="message" class="form-textarea" rows="4" required></textarea>
+                    <!-- Category -->
+                    <div class="container p-4">
+                        <div class="text-center p-3">
+                            <h1>Category</h1>
+                        </div>
+                    </div>
 
-            <button type="submit" class="form-button">Send</button>
-        </form>
-    </div>
+                    <div class="row">
+                        <!-- Product Card 1 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/thinkpad.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
 
-    <div class="contact-text">
-        <div class="text-contact">text</div>
-    </div>
-    <!-- Footer -->
-    <?php include '../module/footer.php' ?>
-</body>
+                        <!-- Product Card 2 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/dell.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- Product Card 3 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/hp.jpg" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- Product Card 4 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/acer.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- Product Card 5 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/lenovo.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- Product Card 6 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/asus.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- Product Card 7 -->
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <a href="https://www.google.com">
+                                    <img src="../assets/binus/infinix.png" class="card-img-top" alt="Product 1">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Footer -->
+                    =======
+                    <!-- Banner Atas -->
+                    <div class="slideshow">
+                        <img class="slides" src="../assets/binus/thinkpad.png">
+                        <img class="slides" src="../assets/binus/hp.jpg">
+                        <img class="slides" src="../assets/binus/lenovo.png">
+                        <img class="slides" src="../assets/binus/acer.png">
+                        <img class="slides" src="../assets/binus/asus.png">
+                        <img class="slides" src="../assets/binus/infinix.png">
+                        <img class="slides" src="../assets/binus/dell.png">
+                    </div>
+                    <script src="../assets/js/script.js"></script>
+
+                    <div class="container-form">
+                        <div class="form-container" style="background: url('../assets/binus/shopbg.jpg') no-repeat center center;">
+                            <h1 class="form-title">Contact Us</h1>
+                            <form id="contact-form" class="contact-form">
+                                <label for="name" class="form-label">Nama Lengkap:</label>
+                                <input type="text" id="name" name="name" class="form-input" required>
+
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" id="email" name="email" class="form-input" required>
+
+                                <label for="message" class="form-label">Jelaskan Laptop Seperti Apa Yang Anda Cari:</label>
+                                <textarea id="message" name="message" class="form-textarea" rows="4" required></textarea>
+
+                                <button type="submit" class="form-button">Send</button>
+                            </form>
+                        </div>
+
+                        <div class="info-text" style="background: url('../assets/binus/shopbg.jpg') no-repeat center center;">
+                            <h2>Informasi Lebih Lanjut</h2>
+                            <p>Jika Anda memerlukan bantuan lebih lanjut, silakan hubungi kami melalui nomor di bawah ini.</p>
+                            <p>
+                                <a href="https://wa.me/8568428849"><img src="../assets/binus/Whatsapp.png" style="width: 30px; height: 30px; margin-right: 10px;">
+                                    +62 856-8428-849
+                                </a>
+                            </p>
+
+                            <p>
+                                <a href="mailto:info@example.com"><img src="../assets/binus/Whatsapp.png" style="width: 30px; height: 30px; margin-right: 10px;">
+                                    +62 838-1234-5678
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                    <!-- Footer -->
+                    >>>>>>> 3941e5cf80c2bf52496ad2c08c6406365931b9c4
+                    <?php include '../module/footer.php' ?>
+        </body>
