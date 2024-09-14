3321b0c243738b379c0bd6b6c489d364059183b3
Tois286 <itoto1937@gmail.com>
2024-09-14 09:48:19 +0700

    stratt

--- view/shop.php
+++ view/shop.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/Randi.css">
 </head>
 
 <body style="background-color: #FFFFFF; background-attachment: fixed; background-size: cover; background-position: center;" background="../assets/binus/shopbg.jpg">
