9ddc9877b82290856a76374a3ca737a17e2af018
Tois286 <itoto1937@gmail.com>
2024-09-14 10:29:24 +0700

    gas

--- module/header.php
+++ module/header.php
@@ -6,7 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Binus Order</title>
+    <link rel="stylesheet" href="assets/css/Randi.css">
     <link rel="stylesheet" href="assets/css/style.css">
+
     <!-- Bootstrap CSS (optional, jika ingin menggunakan) -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
